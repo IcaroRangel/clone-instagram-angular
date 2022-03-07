733052f49b6b827e5bac5209988dd39242bdc4a7
Icaro <icarorangel05@gmail.com>
2022-03-07 19:55:15 -0300

    stories feed

--- src/app/components/stories/stories.component.ts
+++ src/app/components/stories/stories.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-stories',
   templateUrl: './stories.component.html',
-  styleUrls: ['./stories.component.css']
+  styleUrls: ['./stories.component.css'],
 })
 export class StoriesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
