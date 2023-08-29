5603ff425fb3d298b75f5431c963390d6df99aeb
IcaroRangel <icarorangel05@gmail.com>
2023-08-29 15:04:40 -0300

    atualiza stories

--- src/app/components/stories/stories.component.ts
+++ src/app/components/stories/stories.component.ts
@@ -7,22 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoriesComponent {
   public slides = [
-    { img: 'https://picsum.photos/id/18/56/56', user: 'Usuário 1' },
-    { img: 'https://picsum.photos/id/22/56/56', user: 'Usuário 2' },
-    { img: 'https://picsum.photos/id/39/56/56', user: 'Usuário 3' },
-    { img: 'https://picsum.photos/id/41/56/56', user: 'Usuário 4' },
-    { img: 'https://picsum.photos/id/57/56/56', user: 'Usuário 5' },
-    { img: 'https://picsum.photos/id/91/56/56', user: 'Usuário 6' },
-    { img: 'https://picsum.photos/id/107/56/56', user: 'Usuário 7' },
-    { img: 'https://picsum.photos/id/295/56/56', user: 'Usuário 8' },
-    { img: 'https://picsum.photos/id/299/56/56', user: 'Usuário 9' },
-    { img: 'https://picsum.photos/id/301/56/56', user: 'Usuário 10' },
-    { img: 'https://picsum.photos/id/340/56/56', user: 'Usuário 11' },
-    { img: 'https://picsum.photos/id/378/56/56', user: 'Usuário 12' },
+    { img: 'https://picsum.photos/id/18/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/22/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/39/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/41/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/57/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/91/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/107/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/295/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/299/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/301/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/340/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/378/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/400/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/39/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/453/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/471/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/498/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/511/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/524/56/56', user: 'Usuário' },
+    { img: 'https://picsum.photos/id/567/56/56', user: 'Usuário' },
   ];
 
-  public slideConfig = {
-    slidesToShow: 7,
-    slidesToScroll: 2,
-  };
+  public slideConfig = { slidesToShow: 7, slidesToScroll: 2 };
 }
